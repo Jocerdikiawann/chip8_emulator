9859c87390b0f4fb6c78778a3fe95f022532acc6
jocerdikiawann <cocokcocok33@gmail.com>
2025-01-02 23:23:12 +0700

    fix build bat

--- build-raylib-web.bat
+++ build-raylib-web.bat
@@ -1,9 +1,30 @@
-@REM cd build/external/raylib-master/src
+@echo off
 
-@REM mingw32-make PLATFORM=PLATFORM_WEB -B
+set BUILD_RAYLIB=false
 
-@REM cd ../../../../
+:parse_args
+if "%1"=="-with" (
+    if /i "%2"=="raylib" (
+        set BUILD_RAYLIB=true
+    )
+    shift
+)
+shift
+if not "%1"=="" goto parse_args
 
-emcc -o ./web/main.html ./src/main.c ./src/emulator.c ./src/render.c -Os -Wall -Wextra ./build/external/raylib-master/src/libraylib.web.a -I. -I./include/ -I. -I./build/external/raylib-master/src/ -L. -L./build/external/raylib-master/src/ -s USE_GLFW=3 --shell-file ./build/external/raylib-master/src/shell.html -DPLATFORM_WEB --preload-file ./resources/tetris.ch8 --preload-file ./resources/brix.ch8 -s ASYNCIFY
+if %BUILD_RAYLIB%==true (
+    cd build/external/raylib-master/src
+    mingw32-make PLATFORM=PLATFORM_WEB -B
+    cd ../../../../
+)
 
-emrun ./web/main.html
+emcc -o ./web/main.html ./src/main.c ./src/emulator.c ./src/render.c -Os -Wall -Wextra ^
+    ./build/external/raylib-master/src/libraylib.web.a -I. -I./include/ -I. ^
+    -I./build/external/raylib-master/src/ -L. -L./build/external/raylib-master/src/ ^
+    -s USE_GLFW=3 --shell-file ./build/external/raylib-master/src/shell.html -DPLATFORM_WEB ^
+    --preload-file ./resources/tetris.ch8 --preload-file ./resources/brix.ch8 ^
+    -s ASYNCIFY -s FORCE_FILESYTEM=1 --profiling
+
+if %ERRORLEVEL% EQU 0 (
+    start emrun ./web/main.html
+)
